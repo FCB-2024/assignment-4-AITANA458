aa6cc466b6b0f555c0dac181c3ee47c9a3f5b2cd
AITANA LÁZARO <aitana.lazaro01@estudiant.upf.edu>
2024-10-21 19:48:27 +0200

    modified

--- src/antiprime.py
+++ src/antiprime.py
@@ -24,24 +24,27 @@ def main(x) :
 			a=a+1
 		l=l-1
 	if z>=r:
-    	return("not anti-prime")
+		res="not anti-prime"
+    	return(res)
 	else:
-		return("anti-prime")
-
+		res="anti-prime"
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+		return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
+	if len(sys.argv)>1:
+			x=int(sys.argv[1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+print(main(x))
