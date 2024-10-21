3d34f27ee1dab6c2975cb6f71edf03821ef5eec1
AITANA LÁZARO <aitana.lazaro01@estudiant.upf.edu>
2024-10-21 20:14:06 +0200

    modified

--- src/antiprime.py
+++ src/antiprime.py
@@ -4,28 +4,27 @@
 def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-    i=1
-    r=0
+	i = 1
+	r = 0
 
-	while i<=x:
-		if x%i==0:
-			r=r+1
-		i=i+1
-	l=x-1
-	z=0
+	while i <= x:
+		if x % i == 0:
+			r = r + 1
+		i = i + 1
+	l = x - 1
+	z = 0
 
-	while l>=1 and z<r:
-		a=1
-		z=0
-
-		while a<=l:
-			if l%a==0:
-				z=z+1
-			a=a+1
-		l=l-1
-	if z>=r:
+	while l >= 1 and z < r:
+		a = 1
+		z = 0
+		while a <= l:
+			if l % a == 0:
+				z = z + 1
+			a = a + 1
+		l = l - 1
+	if z >= r:
 		res="not anti-prime"
-    	return(res)
+		return("not anti-prime")
 	else:
 		res="anti-prime"
 
@@ -39,12 +38,12 @@ def main(x) :
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
 	import sys
-	if len(sys.argv)>1:
-			x=int(sys.argv[1])
+	if len(sys.argv) > 1:
+		x = int(sys.argv[1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-print(main(x))
+		print(main(x))
